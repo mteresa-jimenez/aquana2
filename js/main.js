f4d0728abac5798480b52e40d89d872c4039bf46
mteresa-jimenez <mteresa.jn@gmail.com>
2021-07-05 23:10:47 +0200

    Add animation to octopus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
+// const pulpito = document.querySelector('#pulpito');
+
+// console.log(pulpito);
+
+// function camouflage() {
+//     pulpito.classList.add("pulpito-camouflage");
+// }
+
+// pulpito.addEventListener("click", camouflage);
